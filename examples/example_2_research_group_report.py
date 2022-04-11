d6764689c324ab72cd773b50ca25a229bc5b03df
Diogo Rato <diogo.rato.11@gmail.com>
2022-04-11 14:54:15 +0100

    Add History Callbacks to example 2

--- examples/example_2_research_group_report.py
+++ examples/example_2_research_group_report.py
@@ -5,8 +5,10 @@ from sciencer import Sciencer
 from sciencer.providers import SemanticScholarProvider
 from sciencer.collectors import CollectByAuthorID
 from sciencer.filters import FilterByYear
+from sciencer.utils import HistoryCallbacks
 
-researchers = ["145136631", "3233831", "1816411", "145813496", "145955156", "145125979", "2151066261", "144518313", "2158393415", "143825592"]
+researchers = ["145136631", "3233831", "1816411", "145813496", "145955156",
+               "145125979", "2151066261", "144518313", "2158393415", "143825592"]
 
 if __name__ == "__main__":
 
@@ -15,8 +17,12 @@ if __name__ == "__main__":
 
     for researcher in researchers:
         s.add_collector(CollectByAuthorID(author_id=researcher))
-    
+
     s.add_filter(FilterByYear(min_year=2022, max_year=2022))
-    
-    for paper in s.iterate( remove_source_from_results=False):
+
+    history_cb = HistoryCallbacks()
+
+    for paper in s.iterate(remove_source_from_results=False, callbacks=[history_cb]):
         print(f"{paper.year}@{paper.paper_id} - {paper.title} => {paper.authors}")
+
+    history_cb.dump()
