d1d85f6b53e2b1d10d3590bbb16bd8e34876de68
Diogo Rato <diogo.rato.11@gmail.com>
2022-04-18 10:50:25 +0100

    Fix author provider tests
    Change researcher to Charles Darwin

--- tests/integration/test_semantic_scholar_provider.py
+++ tests/integration/test_semantic_scholar_provider.py
@@ -1,12 +1,12 @@
 import unittest
-from sciencer.models import Paper, PaperIDs
+from sciencer.models import PaperIDs
 from sciencer.providers import SemanticScholarProvider
 from sciencer.providers.provider import Provider
 
 # Author: Oren Etzioni
 # Visited on 25/03/2022
-author_id = "1741101"
-author_number_papers = 268
+author_id = "40603865"
+author_number_papers = 380
 
 # Papers:
 # Visited on 18/03/2022
@@ -47,15 +47,23 @@ class TestSemanticScholarProvider(unittest.TestCase):
         paper_1_result = self.__provider.get_paper_by_id(paper_1_id)
         self.assertIsNotNone(paper_1_result)
 
-        self.assertEqual(paper_1_result.get_external_id(PaperIDs.LABEL.MAG), paper_1_externalIds["MAG"])
-        self.assertEqual(paper_1_result.get_external_id(PaperIDs.LABEL.ACL), paper_1_externalIds["ACL"])
-        self.assertEqual(paper_1_result.get_external_id(PaperIDs.LABEL.ARXIV), paper_1_externalIds["ArXiv"])
-        self.assertEqual(paper_1_result.get_external_id(PaperIDs.LABEL.DBLP), paper_1_externalIds["DBLP"])
-        self.assertEqual(paper_1_result.get_external_id(PaperIDs.LABEL.DOI), paper_1_externalIds["DOI"])
-        self.assertEqual(paper_1_result.get_external_id(PaperIDs.LABEL.CORPUS), paper_1_externalIds["CorpusId"])
+        self.assertEqual(paper_1_result.get_external_id(
+            PaperIDs.LABEL.MAG), paper_1_externalIds["MAG"])
+        self.assertEqual(paper_1_result.get_external_id(
+            PaperIDs.LABEL.ACL), paper_1_externalIds["ACL"])
+        self.assertEqual(paper_1_result.get_external_id(
+            PaperIDs.LABEL.ARXIV), paper_1_externalIds["ArXiv"])
+        self.assertEqual(paper_1_result.get_external_id(
+            PaperIDs.LABEL.DBLP), paper_1_externalIds["DBLP"])
+        self.assertEqual(paper_1_result.get_external_id(
+            PaperIDs.LABEL.DOI), paper_1_externalIds["DOI"])
+        self.assertEqual(paper_1_result.get_external_id(
+            PaperIDs.LABEL.CORPUS), paper_1_externalIds["CorpusId"])
 
         paper_2_result = self.__provider.get_paper_by_id(paper_2_id)
         self.assertIsNotNone(paper_2_result)
 
-        self.assertEqual(paper_2_result.get_external_id(PaperIDs.LABEL.PUBMED), paper_2_externalIds["PubMed"])
-        self.assertEqual(paper_2_result.get_external_id(PaperIDs.LABEL.CORPUS), paper_2_externalIds["CorpusId"])
+        self.assertEqual(paper_2_result.get_external_id(
+            PaperIDs.LABEL.PUBMED), paper_2_externalIds["PubMed"])
+        self.assertEqual(paper_2_result.get_external_id(
+            PaperIDs.LABEL.CORPUS), paper_2_externalIds["CorpusId"])
