814994d88aa4e524fbf8837d004a12cc04266c26
Diogo Rato <diogo.rato.11@gmail.com>
2022-04-25 10:27:45 +0100

    Handle Empty Field of Study
    Filter has a parameter to define the default value when empty

--- sciencer/filters/filter_by_field_of_study.py
+++ sciencer/filters/filter_by_field_of_study.py
@@ -7,13 +7,14 @@ from ..models import Paper
 class FilterByFieldOfStudy(Filter):
     """Class that encapsulates the filter of paper by its fields of study"""
 
-    def __init__(self, field_of_study: str) -> None:
+    def __init__(self, field_of_study: str, accept_when_empty=False) -> None:
         super().__init__(policies=[])
         self.__field_of_study: str = field_of_study
+        self.__accept_when_empty: bool = accept_when_empty
 
     def is_valid(self, paper: Paper) -> bool:
         if paper.fields_of_study is None:
-            return False
+            return self.__accept_when_empty
         return self.__field_of_study in paper.fields_of_study
 
     def __str__(self) -> str:
