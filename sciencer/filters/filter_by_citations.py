087e60bae4c2b5d19adf8bd7f0b3ea800d738a95
ratuspro <diogo.rato.11@gmail.com>
2022-07-01 10:34:23 +0000

    Add Filter by Number of Citations

---
+++ sciencer/filters/filter_by_citations.py
@@ -0,0 +1,32 @@
+"""Filters paper by its number of citations
+"""
+from .filter import Filter
+from ..models import Paper
+
+
+class FilterByCitations(Filter):
+    """Class that encapsulates the filter of a paper by its number of citations"""
+
+    def __init__(self, min_citations: int, max_citations: int, accept_when_empty=False) -> None:
+        """Create a filter by number of citations.
+
+        Args:
+            min_citations (int): to be accepted, paper's number of citations should be more or \
+                equal to this value
+            max_citations (int): to be accepted, paper's number of citations should be less or \
+                equal to this value
+            accept_when_empty (bool, optional): when True, the Filter accepts papers without the \
+                necessary properties to check validity. Defaults to False.
+        """
+        super().__init__(policies=[], accept_when_empty=accept_when_empty)
+        self.__min: int = min_citations
+        self.__max: int = max_citations
+
+    def is_valid(self, paper: Paper) -> bool:
+        if paper.citations_ids is None:
+            return self._accept_when_empty
+
+        return self.__min <= len(paper.citations_ids) <= self.__max
+
+    def __str__(self) -> str:
+        return f"<FilterByCitations [min: {self.__min}, max: {self.__max}]>"
